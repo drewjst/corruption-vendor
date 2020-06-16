8627a952661943d48612d44929287cfa53c5625e
Andrew S <drewjst@gmail.com>
2020-06-16 15:55:13 -0700

    initialize angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CorruptionComponent } from './corruption/corruption.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CorruptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
